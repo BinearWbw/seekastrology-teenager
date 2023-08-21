47f45425369352ca1b30f39c0a6ed67769cc3d3c
BinearWbw <binear7@gmail.com>
2023-08-21 10:01:37 +0800

    流星初始化时隐藏

--- pages/index.vue
+++ pages/index.vue
@@ -60,6 +60,7 @@ export default {
       border-radius: 50%;
       box-shadow: 0 0 0 4px rgba(255, 255, 255, 0.1),
         0 0 0 4px rgba(255, 255, 255, 0.1), 0 0 10px rgba(255, 255, 255, 1);
+      opacity: 0;
       animation: animate 3s linear infinite;
       z-index: 66;
       &::before {
