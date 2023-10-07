66977714ce43958736f00e0c567f4cdd90001c9e
BinearWbw <binear7@gmail.com>
2023-10-07 18:22:01 +0800

    新增青年/宠物星座页面

---
+++ components/el_components/ElJoinUs.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="join_main">
+    <div class="content">
+      <div class="content_main">
+        <div class="title">Join us</div>
+        <p class="h5 tips">Get your daily horoscope for free</p>
+        <button class="button">
+          <span class="pc">Get your daily horoscope for free</span>
+          <span class="h5">Join now</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:math';
+.join_main {
+  .content {
+    width: 1400px;
+    margin: 0 auto;
+    border-radius: 24px;
+    border: 1px solid rgba(255, 255, 255, 0.4);
+    background: linear-gradient(
+      180deg,
+      rgba(255, 255, 255, 0.08) 0%,
+      rgba(255, 255, 255, 0) 100%
+    );
+    backdrop-filter: blur(8px);
+    padding: 3px;
+    &_main {
+      border-radius: 21px;
+      border: 1px solid rgba(255, 255, 255, 0.4);
+      backdrop-filter: blur(8px);
+      padding: 21px 0 33px;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      .title {
+        color: #fff;
+        text-align: center;
+        font-family: 'Cinzel Decorative';
+        font-size: 36px;
+        font-style: normal;
+        font-weight: 700;
+        line-height: 48px;
+      }
+      .button {
+        margin-top: 16px;
+        color: #000;
+        font-family: 'Rubik';
+        font-size: 22px;
+        font-style: normal;
+        font-weight: 400;
+        line-height: 30px;
+        height: 56px;
+        padding: 8px 120px;
+        border-radius: 42px;
+        background: #fff;
+        cursor: pointer;
+      }
+      .h5 {
+        display: none;
+      }
+    }
+  }
+}
+@media (max-width: 1470px) {
+  .join_main {
+    padding: 0 50px;
+    .content {
+      width: 100%;
+    }
+  }
+}
+@media (max-width: 750px) {
+  $pr: math.div(1vw, 3.75);
+  .join_main {
+    padding: 0;
+    .content {
+      border-radius: 24 * $pr;
+      border: 1 * $pr solid rgba(255, 255, 255, 0.4);
+      backdrop-filter: blur(8 * $pr);
+      padding: 3 * $pr;
+      &_main {
+        border-radius: 21 * $pr;
+        border: 1 * $pr solid rgba(255, 255, 255, 0.4);
+        backdrop-filter: blur(8 * $pr);
+        padding: 21 * $pr 0;
+        .title {
+          font-size: 26 * $pr;
+          line-height: 36 * $pr;
+        }
+        .button {
+          margin-top: 16 * $pr;
+          font-size: 16 * $pr;
+          line-height: 22 * $pr;
+          height: 42 * $pr;
+          padding: 8 * $pr 100 * $pr;
+          border-radius: 42 * $pr;
+          .pc {
+            display: none;
+          }
+        }
+        .h5 {
+          display: block;
+        }
+        .tips {
+          margin-top: 16 * $pr;
+          color: #ffda8b;
+          text-align: center;
+          font-family: 'Rubik';
+          font-size: 16 * $pr;
+          font-style: normal;
+          font-weight: 400;
+          line-height: 22 * $pr;
+        }
+      }
+    }
+  }
+}
+</style>
---
+++ pages/petsign/_id.vue
@@ -0,0 +1,579 @@
+<template>
+  <div class="horroscopes">
+    <div class="horroscopes_top_bg">
+      <div class="imgbg"></div>
+    </div>
+    <google-ad classNames="horroscopes_googlead" :id="'7036206358'"></google-ad>
+    <div class="horroscopes_main">
+      <div class="horoscope_details">
+        <google-ad classNames="google_ad" :id="'4376417914'"></google-ad>
+        <div class="datails_list">
+          <div class="list_top">
+            <h3>{{ toUpperBig(currentName) }} Pet</h3>
+            <div class="pull_down">
+              <el-selected
+                :options="tabList"
+                :pint="ids"
+                @change="handleDropdownChange"
+              ></el-selected>
+            </div>
+          </div>
+          <div class="list_main">
+            <div class="list_tabs">
+              <el-tabs
+                :tabs="horroData"
+                :tabsId="tabsId"
+                @click="correspondingTime"
+              >
+                <template>
+                  <div class="text_form">
+                    <div class="format_date">
+                      {{ $utils.horoscopeFormatDate(currentTime) }}
+                    </div>
+                    <div
+                      class="texts"
+                      :class="{ texts_more: openExpand }"
+                      ref="textMore"
+                      v-html="moreData[comentIds].texts"
+                    ></div>
+                    <div class="more_btn" v-if="aoMore" @click="setOpenExpand">
+                      {{ openExpand ? 'Show Less' : 'Show More' }}
+                    </div>
+                  </div>
+                </template>
+              </el-tabs>
+            </div>
+          </div>
+          <!-- <google-ad classNames="google_ad" :id="'8124091233'"></google-ad> -->
+        </div>
+      </div>
+      <div class="mexplore">
+        <el-explore-more />
+      </div>
+    </div>
+    <transition name="fade">
+      <el-join-us class="joins"></el-join-us>
+    </transition>
+    <div class="google_line">
+      <google-auto-ad classNames="google_ad" :id="'4184846228'" />
+    </div>
+    <transition name="fade">
+      <tarot-more-tarot></tarot-more-tarot>
+    </transition>
+    <transition name="fade">
+      <el-pairing></el-pairing>
+    </transition>
+    <transition name="fade">
+      <el-loading v-if="isLoading"></el-loading>
+    </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PetSign',
+  data() {
+    return {
+      tabList: [
+        {
+          name: 'Aries',
+          imgUrl: require('~/assets/img/home/choice/Aries.png'),
+          id: 1,
+        },
+        {
+          name: 'Taurus',
+          imgUrl: require('~/assets/img/home/choice/Taurus.png'),
+          id: 2,
+        },
+        {
+          name: 'Gemini',
+          imgUrl: require('~/assets/img/home/choice/Gemini.png'),
+          id: 3,
+        },
+        {
+          name: 'Cancer',
+          imgUrl: require('~/assets/img/home/choice/Cancer.png'),
+          id: 4,
+        },
+        {
+          name: 'Leo',
+          imgUrl: require('~/assets/img/home/choice/Leo.png'),
+          id: 5,
+        },
+        {
+          name: 'Virgo',
+          imgUrl: require('~/assets/img/home/choice/Virgo.png'),
+          id: 6,
+        },
+        {
+          name: 'Libra',
+          imgUrl: require('~/assets/img/home/choice/Libra.png'),
+          id: 7,
+        },
+        {
+          name: 'Scorpio',
+          imgUrl: require('~/assets/img/home/choice/Scorpio.png'),
+          id: 8,
+        },
+        {
+          name: 'Sagittarius',
+          imgUrl: require('~/assets/img/home/choice/Sagittarius.png'),
+          id: 9,
+        },
+        {
+          name: 'Capricorn',
+          imgUrl: require('~/assets/img/home/choice/Capricorn.png'),
+          id: 10,
+        },
+        {
+          name: 'Aquarius',
+          imgUrl: require('~/assets/img/home/choice/Aquarius.png'),
+          id: 11,
+        },
+        {
+          name: 'Pisces',
+          imgUrl: require('~/assets/img/home/choice/Pisces.png'),
+          id: 12,
+        },
+      ],
+      horroData: [
+        {
+          tabs: 'Weekly',
+          type: 'w',
+        },
+      ],
+      comentId: 0,
+      currentTime: 'w',
+      comentIdSession: 0,
+      isLoading: false,
+      isSetTimes: null,
+      aoMore: false,
+      openExpand: false,
+      timeout: null,
+    }
+  },
+  async asyncData({ error, $apiList, params }) {
+    try {
+      let ids = 1,
+        currentName = '',
+        comentIds = 0,
+        regey = /-([0-9]+)-/,
+        madeId = null,
+        tabsId = 0
+      let [youlistData] = await Promise.all([
+        $apiList.home
+          .getZodiacHoroscope({
+            origin: process.env.origin,
+            id: params.id.replace(
+              /^.*?(\d*)$/,
+              (str, match, index) => match || '0'
+            ),
+            kind: 'w',
+          })
+          .then((res) => {
+            ids = res.id
+            madeId = regey.exec(params.id)
+            if (madeId && madeId[1] !== '5') {
+              comentIds = madeId[1]
+            }
+            if (madeId && madeId[1] == '5') {
+              tabsId = madeId[1]
+            }
+            currentName = res.name
+            return res.horoscope
+          }),
+      ])
+      return {
+        ids,
+        youlistData,
+        currentName,
+        comentIds,
+        tabsId,
+      }
+    } catch (e) {
+      error({ statusCode: e.code, message: e.message })
+    }
+  },
+  computed: {
+    moreData() {
+      return [
+        {
+          name: 'Daily',
+          title: 'Daily Horoscope',
+          imgUrl: require('~/assets/img/horroscope/daily_horoscope.png'),
+          texts: this.youlistData?.general,
+        },
+        {
+          name: 'Love',
+          title: 'Love Horoscope',
+          imgUrl: require('~/assets/img/horroscope/love_horoscope.png'),
+          texts: this.youlistData?.love,
+        },
+        {
+          name: 'Health',
+          title: 'Health Horoscope',
+          imgUrl: require('~/assets/img/horroscope/health_horoscope.png'),
+          texts: this.youlistData?.health,
+        },
+        {
+          name: 'Career',
+          title: 'Career Horoscope',
+          imgUrl: require('~/assets/img/horroscope/career_horoscope.png'),
+          texts: this.youlistData?.career,
+        },
+        {
+          name: 'Money',
+          title: 'Money Horoscope',
+          imgUrl: require('~/assets/img/horroscope/money_horoscope.png'),
+          texts: this.youlistData?.finances,
+        },
+      ]
+    },
+  },
+  mounted() {
+    this.getCurrentTexts()
+  },
+  methods: {
+    async getHoroscopeData(id = 1, kind = 'w') {
+      this.isLoading = true
+      await this.$apiList.home
+        .getZodiacHoroscope({
+          origin: process.env.origin,
+          id,
+          kind,
+        })
+        .then((res) => {
+          this.isLoading = false
+          this.youlistData = res.horoscope
+          this.getCurrentTexts()
+        })
+    },
+    handleDropdownChange(option) {
+      // 点击下拉框
+      this.ids = option.id
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth',
+      })
+      window.changePageUrl = `/petsign/${option.name
+        .replace(/[^a-zA-Z0-9\\s]/g, '-')
+        .toLowerCase()}-${option.id}/`
+      window.location.href = `/petsign/${option.name
+        .replace(/[^a-zA-Z0-9\\s]/g, '-')
+        .toLowerCase()}-${option.id}/`
+      this.isLoading = true
+    },
+    setCorresponding(i) {
+      // 点击其他运势
+      this.$nextTick(() => {
+        this.isLoading = true
+        this.isSetTimes = setTimeout(() => {
+          this.comentIds = i
+          this.isLoading = false
+          window.scrollTo({
+            top: 0,
+            behavior: 'smooth',
+          })
+        }, 500)
+        this.getCurrentTexts()
+      })
+    },
+    correspondingTime(i) {
+      // 点击tabs
+      this.currentTime = i.type
+      this.getHoroscopeData(this.ids, i.type)
+      this.openExpand = false
+    },
+    toUpperBig(str) {
+      return str?.charAt(0).toUpperCase() + str?.slice(1)
+    },
+    getCurrentTexts() {
+      this.timeout = setTimeout(() => {
+        const moreConten = this.$refs.textMore
+        const aoMore = moreConten.scrollHeight > moreConten.offsetHeight
+        this.aoMore = aoMore
+      }, 500)
+    },
+    setOpenExpand() {
+      this.openExpand = !this.openExpand
+      if (!this.openExpand) {
+        window.scrollTo({
+          top: 0,
+          behavior: 'smooth',
+        })
+      }
+    },
+  },
+  destroyed() {
+    if (this.isSetTimes) clearTimeout(this.isSetTimes)
+    if (this.timeout) clearTimeout(this.timeout)
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+@use 'sass:math';
+.horroscopes {
+  position: relative;
+  &_googlead {
+    display: none;
+  }
+  &_top_bg {
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 1400px;
+    flex-shrink: 0;
+    .imgbg {
+      width: 100%;
+      height: 420px;
+      background: url('~/assets/img/teenager/animal_sign.webp') no-repeat;
+      background-size: cover;
+    }
+  }
+  &_main {
+    margin: 0 auto 48px;
+    width: 1400px;
+    .horoscope_details {
+      display: flex;
+      padding: 288px 0 48px;
+      .google_ad {
+        width: 300px;
+        height: 250px;
+      }
+      .datails_list {
+        flex: 1;
+        padding-left: 54px;
+        .list_top {
+          display: flex;
+          justify-content: space-between;
+          margin-bottom: 24px;
+          align-items: center;
+          h3 {
+            font-family: 'Cinzel Decorative';
+            font-style: normal;
+            font-weight: 700;
+            font-size: 36px;
+            line-height: 48px;
+            color: #ffffff;
+            position: relative;
+          }
+          .pull_down {
+            width: 338px;
+            height: 60px;
+          }
+        }
+        .list_main {
+          width: 100%;
+          .list_tabs {
+            width: 100%;
+            .text_form {
+              padding-top: 24px;
+              .format_date {
+                display: inline;
+                font-family: 'Rubik Medium';
+                font-style: normal;
+                font-size: 16px;
+                line-height: 28px;
+                color: #9747ff;
+              }
+              .texts {
+                font-family: 'Rubik';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 28px;
+                color: rgba(255, 255, 255, 0.6);
+                text-overflow: ellipsis;
+                overflow: hidden;
+                display: -webkit-box;
+                -webkit-line-clamp: 8;
+                -webkit-box-orient: vertical;
+                ::v-deep(:first-child) {
+                  display: inline;
+                }
+              }
+              .texts_more {
+                -webkit-line-clamp: unset !important;
+              }
+              .more_btn {
+                font-family: 'Rubik';
+                font-style: normal;
+                font-weight: 400;
+                font-size: 16px;
+                line-height: 28px;
+                color: #9747ff;
+                cursor: pointer;
+              }
+            }
+          }
+        }
+        .google_ad {
+          width: 924px;
+          min-height: 130px;
+          margin: 48px auto 0;
+        }
+      }
+    }
+    .mexplore {
+      padding-top: 48px;
+    }
+  }
+  .joins {
+    padding-top: 48px;
+  }
+  .google_line {
+    padding: 96px 0;
+    .google_ad {
+      width: 1200px;
+      margin: 0 auto;
+    }
+  }
+}
+@media (max-width: 1470px) {
+  .horroscopes {
+    &_main {
+      width: 100%;
+      padding: 0 60px;
+      .horoscope_details {
+        .datails_list {
+          padding-left: 30px;
+          .google_ad {
+            width: 100%;
+          }
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 1350px) {
+  .horroscopes {
+    .google_line {
+      padding: 96px 30px;
+      .google_ad {
+        max-width: 100%;
+      }
+    }
+  }
+}
+
+@media (max-width: 1000px) {
+  .horroscopes {
+    &_main {
+      .horoscope_details {
+        > .google_ad {
+          display: none;
+        }
+        .datails_list {
+          padding-left: 0;
+        }
+      }
+    }
+    .google_line {
+      .google_ad {
+        width: 100%;
+      }
+    }
+  }
+}
+
+@media (max-width: 750px) {
+  $pr: math.div(1vw, 3.75);
+  .horroscopes {
+    padding-top: 96 * $pr;
+    &_googlead {
+      display: block;
+      width: 343 * $pr;
+      height: 110 * $pr;
+      margin: 0 auto 24 * $pr;
+      position: relative;
+    }
+    &_top_bg {
+      width: 450 * $pr;
+      .imgbg {
+        width: 100%;
+        height: 135 * $pr;
+      }
+    }
+    &_main {
+      width: 100%;
+      padding: 0 16 * $pr;
+      margin: 0 auto 48 * $pr;
+      .horoscope_details {
+        display: flex;
+        padding: 24 * $pr 0;
+        position: relative;
+        &::after {
+          height: 1 * $pr;
+        }
+        > .google_ad {
+          display: none;
+        }
+        .datails_list {
+          width: 100%;
+          flex: 1;
+          padding-left: 0;
+          .list_top {
+            display: flex;
+            justify-content: initial;
+            flex-direction: column-reverse;
+            margin-bottom: 24 * $pr;
+            h3 {
+              font-size: 26 * $pr;
+              line-height: 36 * $pr;
+              text-align: center;
+            }
+            .pull_down {
+              width: 100%;
+              height: auto;
+              margin-bottom: 24 * $pr;
+            }
+          }
+          .list_main {
+            width: 100%;
+            .list_tabs {
+              .text_form {
+                padding-top: 12 * $pr;
+                .format_date {
+                  font-size: 16 * $pr;
+                  line-height: 28 * $pr;
+                }
+                .texts {
+                  font-size: 16 * $pr;
+                  line-height: 28 * $pr;
+                  height: 224 * $pr;
+                }
+                .texts_more {
+                  -webkit-line-clamp: unset !important;
+                  height: auto !important;
+                }
+                .more_btn {
+                  font-size: 16 * $pr;
+                  line-height: 28 * $pr;
+                }
+              }
+            }
+          }
+          .google_ad {
+            display: none;
+            width: 100%;
+            height: 299 * $pr;
+            margin: 23 * $pr auto 0;
+          }
+        }
+      }
+      .mexplore {
+        padding-top: 48 * $pr;
+      }
+    }
+    .joins {
+      padding: 0 16 * $pr 48 * $pr;
+    }
+    .google_line {
+      padding: 0 16 * $pr 48 * $pr;
+    }
+  }
+}
+</style>
