133ba91c7a8dc52da5b70291ca9c733c02c2cbf6
BinearWbw <binear7@gmail.com>
2024-01-12 17:52:49 +0800

    资讯组件改成自动轮播

--- components/el_components/ElWidget.vue
+++ components/el_components/ElWidget.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="integrated">
     <!-- 资讯/视频/测验-整合组件 -->
-    <home-tabs :tabs="horroData" @click-selected="handleTabSelected">
+    <!-- <home-tabs :tabs="horroData" @click-selected="handleTabSelected">
       <template>
         <home-infor-module
           v-if="selectedTabIndex === 0"
@@ -19,7 +19,38 @@
           :inforData="quizData"
         ></home-infor-module>
       </template>
-    </home-tabs>
+    </home-tabs> -->
+    <div class="tabs_btn">
+      <button
+        :class="['tab-item', { active: selectedTabIndex === index }]"
+        v-for="(i, index) in horroData"
+        :key="index"
+        @click="handleTabSelected(index)"
+      >
+        {{ i.tabs }}
+      </button>
+    </div>
+    <client-only>
+      <a-carousel
+        autoplay
+        :dots="false"
+        ref="carousTab"
+        :afterChange="tabsPanelCallback"
+      >
+        <home-infor-module
+          title="Article"
+          :inforData="articleData"
+        ></home-infor-module>
+        <home-infor-module
+          title="Video"
+          :inforData="videoData"
+        ></home-infor-module>
+        <home-infor-module
+          title="Quiz"
+          :inforData="quizData"
+        ></home-infor-module>
+      </a-carousel>
+    </client-only>
   </div>
 </template>
 
@@ -83,13 +114,19 @@ export default {
   },
   methods: {
     handleTabSelected(index) {
-      this.selectedTabIndex = index
+      //   this.selectedTabIndex = index
+      const carouselDom = this.$refs.carousTab
+      carouselDom.goTo(index)
+    },
+    tabsPanelCallback(to) {
+      this.selectedTabIndex = to
     },
   },
 }
 </script>
 
 <style lang="scss" scoped>
+@use 'sass:math';
 .integrated {
   width: 100%;
   :deep(.tab-header_main) {
@@ -99,6 +136,78 @@ export default {
     width: 100%;
     .title {
       display: none;
+    }
+  }
+  .tabs_btn {
+    width: 100%;
+    display: flex;
+    overflow-x: auto;
+    justify-content: center;
+    white-space: nowrap;
+    padding-bottom: 15px;
+    position: relative;
+    .tab-item {
+      flex: 0 0 auto;
+      padding: 7px 22px;
+      text-align: center;
+      font-family: 'Rubik';
+      font-size: 14px;
+      line-height: 18px;
+      color: rgba(255, 255, 255, 0.6);
+      border-radius: 42px;
+      margin-right: 24px;
+      cursor: pointer;
+      transition: background-color 0.3s ease-in-out;
+      &:hover {
+        background-color: rgba(255, 255, 255, 0.1);
+      }
+    }
+    & > :last-child {
+      margin-right: 0;
+    }
+    .tab-item.active {
+      background-color: #fff;
+      color: #000;
+      font-family: Cinzel Decorative;
+    }
+    &::after {
+      content: '';
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 1px;
+      background: linear-gradient(
+        90deg,
+        rgba(255, 255, 255, 0) 0%,
+        rgba(255, 255, 255, 0.2) 50.52%,
+        rgba(255, 255, 255, 0) 100%
+      );
+    }
+  }
+}
+@media (max-width: 750px) {
+  $pr: math.div(1vw, 3.75);
+  .integrated {
+    width: 343 * $pr;
+
+    .tabs_btn {
+      width: 343 * $pr;
+      padding-bottom: 15 * $pr;
+      .tab-item {
+        padding: 7 * $pr 22 * $pr;
+        font-size: 14 * $pr;
+        line-height: 18 * $pr;
+        border-radius: 42 * $pr;
+        margin-right: 24 * $pr;
+      }
+      & > :last-child {
+        margin-right: 0;
+      }
+      &::after {
+        width: 100%;
+        height: 1 * $pr;
+      }
     }
   }
 }
